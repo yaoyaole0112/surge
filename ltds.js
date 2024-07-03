124b5544dc5cf00dca71e823e1d3fd3aeb57f7ef
yaoyaole0112 <167995186+yaoyaole0112@users.noreply.github.com>
2024-07-03 13:17:43 +0800

    Update ltds.js

--- ltds.js
+++ ltds.js
@@ -5,16 +5,16 @@ async function operator(proxies = []) {
       _.set(p, 'name', ''+name)
       if(_.get(p, 'type') === 'vmess') {
         if(_.get(p, 'network') === 'ws') {
-          _.set(p, 'ws-opts.headers.Host', 'listen.10155.com')
+          _.set(p, 'ws-opts.headers.Host', 'NetworkPanelApi://listen.10155.com')
         }
         if(_.get(p, 'network') === 'http') {
           _.set(p, 'http-opts.headers.method', 'GET')
           _.set(p, 'http-opts.path', ['/'])
-          _.set(p, 'http-opts.headers.Host', ['listen.10155.com'])
+          _.set(p, 'http-opts.headers.Host', ['NetworkPanelApi://listen.10155.com'])
         }
       }
       if(_.get(p, 'type') === 'trojan') {
-        _.set(p, 'sni', 'listen.10155.com')
+        _.set(p, 'sni', 'NetworkPanelApi://listen.10155.com')
       }
       return p
     })
