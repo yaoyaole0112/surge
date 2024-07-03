d3fae7cb2206151a621ddf751e20bdb0cb66c877
yaoyaole0112 <167995186+yaoyaole0112@users.noreply.github.com>
2024-07-03 13:09:00 +0800

    Update and rename ltdsgm.js to ltds.js

--- ltds.js
+++ ltds.js
@@ -2,7 +2,7 @@ async function operator(proxies = []) {
     const _ = lodash
     return proxies.map((p = {}) => {
       const name = _.get(p, 'name') || ''
-      _.set(p, 'name', '[公免]'+name)
+      _.set(p, 'name', ''+name)
       if(_.get(p, 'type') === 'vmess') {
         if(_.get(p, 'network') === 'ws') {
           _.set(p, 'ws-opts.headers.Host', 'listen.10155.com')
